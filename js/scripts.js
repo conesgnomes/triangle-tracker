f1202853f34c901891b9ee2f11011b048065f29c
Connor Larsen <Connor@Cons-Mac.local>
2017-04-27 21:24:53 -0700

    add additional js logic to finish up problem.

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,32 @@ $(function() {
     var sidetwo = parseInt($("input#sidetwo").val());
     var sidethree = parseInt($("input#sidethree").val());
 
-    if (sideone === sidetwo === sidethree) {
+    if ((sideone === sidetwo) && (sidetwo) === sidethree) {
     $("#equilateral").show();
     $("#isosceles").hide();
     $("#scalene").hide();
+    $("#not-a-triangle").hide();
+    }
 
+    else if ((sideone === sidetwo) || (sidetwo === sidethree) || (sideone === sidethree)) {
+    $("#equilateral").hide();
+    $("#isosceles").show();
+    $("#scalene").hide();
+    $("#not-a-triangle").hide();
+    }
+
+    else if ((sideone + sidetwo <= sidethree) || (sideone + sidethree <= sidetwo) || (sidetwo + sidethree <= sideone)) {
+    $("#equilateral").hide();
+    $("#isosceles").hide();
+    $("#scalene").hide();
+    $("#not-a-triangle").show();
+    }
+
+    else {
+    $("#equilateral").hide();
+    $("#isosceles").hide();
+    $("#scalene").show();
+    $("#not-a-triangle").hide();
     }
   event.preventDefault();
   });
